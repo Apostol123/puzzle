e22b2e43f901adfe56891d108553573de541ec38
Apostol_tavernes <apostol516@gmail.com>
2019-02-04 07:33:24 +0100

    puzzle random cargado cambio en el array de direcciones de imagenes de json a array normal

--- main.js
+++ main.js
@@ -1,111 +1,49 @@
+var array_homer_partido=[ "/imagenes/homer_partido/homer_entero_swap_part1x1.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part1x2.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part1x3.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part1x4.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part2x1.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part2x2.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part2x3.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part2x4.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part3x1.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part3x2.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part3x3.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part3x4.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part4x1.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part4x2.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part4x3.jpg",
+"/imagenes/homer_partido/homer_entero_swap_part4x4.jpg"
+];
 
-var homer_partido={
-    "imagen": [{
-        "parte": "/imagenes/homer_partido/homer_entero_swap_part1x1.jpg"
-       
-    },
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part1x2.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part1x3.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part1x4.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part2x1.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part2x2.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part2x3.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part2x4.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part3x1.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part3x2.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part3x3.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part3x4.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part4x1.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part4x2.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part4x3.jpg"}
-    ,
-    {  "parte": "/imagenes/homer_partido/homer_entero_swap_part4x4.jpg"}
+
+
+var array_puzzle_partido=[
+    "/imagenes/puzzle_partido/puzzle_swap_part1x1.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part1x2.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part1x3.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part1x4.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part2x1.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part2x2.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part2x3.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part2x4.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part3x1.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part3x2.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part3x3.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part3x4.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part4x1.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part4x2.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part4x3.jpg",
+    "/imagenes/puzzle_partido/puzzle_swap_part4x4.jpg"
 ]
-}
-
-var puzzle_partido={
-    "imagen":[{
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part1x1.jpg"
-    },
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part1x2.jpg"
-    },
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part1x3.jpg"
-    },
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part1x4.jpg"
-    },
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part2x1.jpg"
-    }
-    ,
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part2x2.jpg"
-    },
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part2x3.jpg"
-    },
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part2x4.jpg"
-    },
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part3x1.jpg"
-    },
-
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part3x2.jpg"
-    }
-    ,
-
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part3x3.jpg"
-    }
-    ,
-
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part3x4.jpg"
-    },
-
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part4x1.jpg"
-    },
-
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part4x2.jpg"
-    }
-    ,
-
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part4x3.jpg"
-    }
-    ,
-
-    {
-        "parte":"/imagenes/puzzle_partido/puzzle_swap_part4x4.jpg"
-    }
-]
-}
 
 
-    load_puzzle(puzzle_partido)
 
-    document.getElementById("start").onclick=function(){
-     load_puzzle_random(homer_partido);
-    };
+
+    load_puzzle_from_array(array_puzzle_partido);
+ 
+
+  
    
 
 
@@ -113,36 +51,50 @@ $(document).ready(function(){
   
     $(".sim").click(function(){
         $("#puzzle").empty();
-        load_puzzle(homer_partido);
+        load_puzzle_from_array(array_homer_partido);
     });
+
+    $("#start").click(function () {
+        $("#puzzle").empty();
+        load_random_puzzle(array_homer_partido);
+      });
+
+
+      $(".grif").click(function () {
+        $("#puzzle").empty();
+        load_puzzle_from_array(array_puzzle_partido);
+      });
 
 
 })
 
-function load_puzzle(puzzle_json){
-   
-    puzzle_json.imagen.forEach(function(obj){
+function load_puzzle_from_array(array_puzzle){
+    array_puzzle.sort();
+    for(var i =0;i<array_puzzle.length;i++){
         var img = new Image();
-        img.src=obj.parte;
+        img.src=array_puzzle[i];
         img.setAttribute("class","border");
         img.setAttribute("height","95");
         img.setAttribute("width","95");
         document.getElementById("puzzle").appendChild(img);
-    })
-    }
 
-    function load_puzzle_random(puzzle_json){
-        var values= Object.values(puzzle_json);
-      
-        for(var i =0 ;i<values.length;i++){
-            var img = new Image();
-            
-            img.src=values[parseInt(Math.random()*values.length)];
-            img.setAttribute("class","border");
-            img.setAttribute("height","95");
-            img.setAttribute("width","95");
-            document.getElementById("puzzle").appendChild(img);
-        }
+    }
+}
+
+
+
+   function load_random_puzzle(array_puzzle){
+        var ri = Math.floor(Math.random() * array_puzzle.length);
+        array_puzzle[ri]="";
+       array_puzzle.sort(function(a, b){return 0.5 - Math.random()});
+    for(var i =0;i<array_puzzle.length;i++){
+        var img = new Image();
+        img.src=array_puzzle[i];
+        img.setAttribute("class","border");
+        img.setAttribute("height","95");
+        img.setAttribute("width","95");
+        document.getElementById("puzzle").appendChild(img);
+
+    }
    
-     
-        }
+   }
