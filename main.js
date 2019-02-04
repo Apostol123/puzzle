ec05a1173dba8c7ae42ff990c62f75a9aabc0bea
Apostol_tavernes <apostol516@gmail.com>
2019-02-04 16:42:45 +0100

    intentando indentificar que elementos se pueden mover

--- main.js
+++ main.js
@@ -42,18 +42,18 @@ var array_puzzle_partido=[
 
 
 
-
+var ri=0;
+var erased_value="";
 
 
     load_puzzle_from_array(array_puzzle_partido);
  
-    var ri=0;
-    var erased_value="";
+  
   
    
 
 
-$(document).ready(function(){
+
   
     $(".sim").click(function(){
         $("#puzzle").empty();
@@ -63,6 +63,7 @@ $(document).ready(function(){
     $("#start").click(function () {
         $("#puzzle").empty();
         load_random_puzzle(array_homer_partido);
+        incializar_partes();
       });
 
 
@@ -71,11 +72,20 @@ $(document).ready(function(){
         load_puzzle_from_array(array_puzzle_partido);
       });
 
+      function calcular_si_mueve(element){
+          alert(element.getAttribute("src")+" y "+$(".vacio").attr("class"));
+          
+        if(element.left==$(".vacio").right){
+                
+        }
+      }
 
-})
+
+
 
 function load_puzzle_from_array(array_puzzle){
-   
+   // compruebo si el array tiene un vacio
+   // de ser asi 
    if(array_puzzle.indexOf("")>-1){
     array_puzzle[array_puzzle.indexOf("")]=erased_value;
    }
@@ -96,16 +106,28 @@ function load_puzzle_from_array(array_puzzle){
 
 
    function load_random_puzzle(array_puzzle){
-       
+       // comprueba que no haya un espacio de ser asi 
+       // genero un numero random
+       // dentro del length de mi array 
+       // y le quito la propiedad
         ri = Math.floor(Math.random() * array_puzzle.length);
        if(!array_puzzle.includes("")){
            erased_value=array_puzzle[ri];
-           alert(erased_value);
+      
         array_puzzle[ri]="";
        }
       
        array_puzzle.sort(function(a, b){return 0.5 - Math.random()});
     for(var i =0;i<array_puzzle.length;i++){
+        if( array_puzzle[i]==""){
+            var img = new Image();
+            img.src=array_puzzle[i];
+            img.setAttribute("class","border");
+            img.setAttribute("class","vacio");
+            img.setAttribute("height","95");
+            img.setAttribute("width","95");
+            document.getElementById("puzzle").appendChild(img);
+        }
         var img = new Image();
         img.src=array_puzzle[i];
         img.setAttribute("class","border");
@@ -116,3 +138,19 @@ function load_puzzle_from_array(array_puzzle){
     }
    
    }
+
+   function incializar_partes(){
+       var partes = document.getElementById("puzzle").childNodes;
+      
+       for (let index = 0; index < partes.length; index++) {
+           
+          partes[index].addEventListener("click",function(){
+             calcular_si_mueve(partes[index]);
+          })
+           
+       }
+   }
+
+
+
+ 
